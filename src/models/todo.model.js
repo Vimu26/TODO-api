0918a0a3cc0f239b938f51a17eb486ef1b0e973a
akalanka <akalanka@echonlabs.com>
2023-11-04 08:22:00 +0530

    bugs fixed

--- src/models/todo.model.js
+++ src/models/todo.model.js
@@ -1,21 +1,21 @@
 const { Schema, model } = require("mongoose");
 
 const todoSchema = new Schema({
-    task : {
-        type: String,
-        required: true,
-    },
-    start_date : {
-        type: Date,
-        required: true,
-        default: new Date,
-    },
-    due_date : {
-        type: Date,
-    },
-    description : {
-        type: String,
-    },
+  task: {
+    type: String,
+    required: true,
+  },
+  start_date: {
+    type: Date,
+    required: true,
+    default: new Date(),
+  },
+  due_date: {
+    type: Date,
+  },
+  description: {
+    type: String,
+  },
 });
 
-module.exports = new model('tasks' , todoSchema);
+module.exports = new model("tasks", todoSchema);
--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const todoController = require("../controllers/todo.controller")
+const todoController = require("../controllers/todo.controller");
 
-router.get("/", todoController.getAll )
-router.post("/", todoController.create )
-router.patch("/:id", todoController.update )
-router.delete("/:id", todoController.delete )
+router.get("/", todoController.getAll);
+router.post("/", todoController.create);
+router.patch("/:id", todoController.update);
+router.delete("/:id", todoController.delete);
 
 module.exports = router;
